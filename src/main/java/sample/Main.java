6ea01521f4ae98410fbc9c885a2d37ef40842e42
Adrian Wieczorek <adrian.wieczorek@tecna.pl>
2017-11-19 20:07:17 +0100

    App version 1.0

---
+++ src/main/java/sample/Main.java
@@ -0,0 +1,38 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/FXML/homeFXML.fxml"));
+        primaryStage.setTitle("Referee Cast System Manager");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setResizable(false);
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.centerOnScreen();
+
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public static void showAlert(Alert.AlertType alertType, String title, String headerText, String contentText){
+        Alert alert = new Alert(alertType);
+        alert.initStyle(StageStyle.UNDECORATED);
+        alert.setTitle(title);
+        alert.setHeaderText(headerText);
+        alert.setContentText(contentText);
+        alert.show();
+    }
+}
